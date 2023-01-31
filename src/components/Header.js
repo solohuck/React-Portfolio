3417f9d8a6a1e3f8429bd8f5489a0fbc53b0af32
solo <solohuckstep@gmail.com>
2023-01-30 20:26:57 -0500

    added src folder, components folder with some basic files and an app.js

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+export function Header() {
+    return {
+        <div>
+            <h1>Solo</h1>
+            <nav>
+                <ul>
+                    <li>item 1</li>
+                    <li>item 2</li>
+                    <li>item 3</li>
+                </ul>
+            </nav>
+        </div>
+    }
+}
