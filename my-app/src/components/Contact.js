e5e5982823f3ab51e20435733cd0fff5bd211205
solo <solohuckstep@gmail.com>
2023-08-01 16:56:54 -0400

    finished styles for contact and about me

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -19,7 +19,21 @@ const Contact = () => {
     return (
         <section id="contact" className="contact-hero">
             <div className="contact__left-box">
-                <h1 className="contact__title">Contact Me</h1>
+                <div className="contact__main-cont"> 
+                    <h1 className="contact__title">About Me</h1>
+                    <div className="contact__bio-cont">
+                        <p className="contact__bio-text">I am a full-stack web developer with a mission to create beautiful, responsive, and user-friendly websites. I originally went to IUPUI university for Music Technology but decided to change my career path to full-stack web development. </p>
+                    </div>
+                </div>
+                <div className="contact__bio-skills-cont">
+                    <ul className="contact__bio-skills-list">
+                        <li className="contact__bio-skills-list-item">HTML</li>
+                        <li className="contact__bio-skills-list-item">CSS</li>
+                        <li className="contact__bio-skills-list-item">JavaScript</li>
+                        <li className="contact__bio-skills-list-item">React</li>
+                        <li className="contact__bio-skills-list-item">Node.js</li>
+                    </ul>
+                </div>
             </div>
             <div className="contact__right-box">
                 <form className="contact__form not-show">
