ff7f0d1137d7dc4a6f2c65797572261e3037aeeb
solo <solohuckstep@gmail.com>
2023-08-02 11:58:55 -0400

    updated the headers css code

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -5,15 +5,15 @@ import '../styles/Header.css';
 const Header = () => {
   
   return (
-    <header className="header__cont">
-        <div className="header__logo-cont">
+    <header className="header">
+        <div className="header__logo-container">
           <span className="header__logo">SolomonHuckstep
           <span className='header__logo-dot'>.</span>
           <span className='header__logo-dash'> _</span>
           </span>
         </div>
         <div>
-          <ul className="header__items-cont">
+          <ul className="header__items-container">
             <li className='header__item'><a href="./">Home</a></li>
             <li className='header__item'> <a href="#projects">projects</a></li>
             <li className='header__item'><a href="#contact">Contact</a></li>
