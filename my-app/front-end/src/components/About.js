7a803068409776d609f8beaa1b1fc3d283ae2ec4
solo <solohuckstep@gmail.com>
2023-09-05 16:29:53 -0400

    adjusted about section for laptop 1024px

--- my-app/front-end/src/components/About.js
+++ my-app/front-end/src/components/About.js
@@ -11,30 +11,33 @@ const About = () =>{
                     Here you will find more information about me, what I do, and
                     my current skills mostly in terms of programming and technology
                 </div>
-                <div className='text__primary-about'>
-                <div className='text__title'> Get to know me! </div>
-                    <p className='text__space'> 
-                        I'm a Frontend Web Developer building the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section. 
-                    </p> 
-                    <p className='text__space'> 
-                        I also like sharing content related to the stuff that I have learned over the years in Web Development so it can help other people of the Dev Community. Feel free to Connect or Follow me on my Linkedin where I post useful content related to Web Development and Programming 
-                    </p> 
-                    <p className='text__space'> 
-                        I'm open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don't hesitate to contact me. 
-                    </p> 
-                    <a href="./#contact"className='contact__btn'>Contact </a>
-                </div>
-                <div className='my__skills'>
-                    <div className='skill__title'> My Skills</div>
-                    <ul className='skills__content'>
-                        <li className='single__skill'>HTML</li>
-                        <li className='single__skill'>CSS</li>
-                        <li className='single__skill'>JavaScript</li>
-                        <li className='single__skill'>React</li>
-                        <li className='single__skill'>GIT</li>
-                        <li className='single__skill'>Github</li>
-                        <li className='single__skill'>Responsive Design</li>
-                    </ul>
+                <div className='about__container'>
+                    <div className='text__primary-about'>
+                        <div className='text__title'> Get to know me! </div>
+                        <p className='text__space'> 
+                            I'm a Frontend Web Developer building the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section. 
+                        </p> 
+                        <p className='text__space'> 
+                            I also like sharing content related to the stuff that I have learned over the years in Web Development so it can help other people of the Dev Community. Feel free to Connect or Follow me on my Linkedin where I post useful content related to Web Development and Programming 
+                        </p> 
+                        <p className='text__space'> 
+                            I'm open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don't hesitate to contact me. 
+                        </p> 
+                        <a href="./#contact"className='contact__btn'>Contact </a>
+                    </div>
+
+                    <div className='my__skills'>
+                        <div className='skill__title'> My Skills</div>
+                        <ul className='skills__content'>
+                            <li className='single__skill'>HTML</li>
+                            <li className='single__skill'>CSS</li>
+                            <li className='single__skill'>JavaScript</li>
+                            <li className='single__skill'>React</li>
+                            <li className='single__skill'>GIT</li>
+                            <li className='single__skill'>Github</li>
+                            <li className='single__skill'>Responsive Design</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
--- my-app/front-end/src/components/Footer.js
+++ my-app/front-end/src/components/Footer.js
@@ -7,20 +7,27 @@ const Footer = () => {
     return (
     <section className="footer" id="footer">
         <div className="footer__content">
-            <div className="social__title">Social</div>
-            <ul className="social__icon">
-                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/solomon-huckstep-461155232/s">
-                    <img className="main-footer__icon" src="https://d33wubrfki0l68.cloudfront.net/7f29579dde49e02480372aa49f7189c5536b0118/34b92/assets/png/linkedin-ico.png" alt="Ram Maheshwari Linkedin Profile"></img>
-                </a>
-                <a target="_blank" rel="noreferrer" href="https://github.com/solohuck">
-                    <img className="main-footer__icon" src="https://d33wubrfki0l68.cloudfront.net/5557d5a11584d7201a38ee1a95200f57a4cc0f88/15085/assets/png/github-ico.png" alt="RammCodes Github Profile"></img>
-                </a>    
-            </ul>
-            <div className="myname">Solomon Huckstep</div>
-            <p className="my__description">
-                A Frontend focused Web Developer building the Frontend of Websites 
-                and Web Applications that leads to the success of the overall product
-            </p>
+            <div className="footer__container">
+                <div className="social__container">
+                    <div className="social__title">Social</div>
+                    <ul className="social__icon">
+                        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/solomon-huckstep-461155232/s">
+                            <img className="main-footer__icon" src="https://d33wubrfki0l68.cloudfront.net/7f29579dde49e02480372aa49f7189c5536b0118/34b92/assets/png/linkedin-ico.png" alt="Ram Maheshwari Linkedin Profile"></img>
+                        </a>
+                        <a target="_blank" rel="noreferrer" href="https://github.com/solohuck">
+                            <img className="main-footer__icon" src="https://d33wubrfki0l68.cloudfront.net/5557d5a11584d7201a38ee1a95200f57a4cc0f88/15085/assets/png/github-ico.png" alt="RammCodes Github Profile"></img>
+                        </a>    
+                    </ul>
+                </div>
+                <div className="myname__container">
+                    <div className="myname">Solomon Huckstep</div>
+                    <p className="my__description">
+                        A Frontend focused Web Developer building the Frontend of Websites 
+                        and Web Applications that leads to the success of the overall product
+                    </p>
+                </div>
+            </div>
+            
             <div className="footer__line">
                 <div className="copyright">
                     © Copyright 2023. Made by <a href="./" className="name__link">Solomon Huckstep</a>
