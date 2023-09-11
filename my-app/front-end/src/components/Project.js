564cc325f9944f0c0a3afa51b937dbb1eeff75b9
solo <solohuckstep@gmail.com>
2023-09-11 14:18:43 -0400

    more updates

--- my-app/front-end/src/components/Project.js
+++ my-app/front-end/src/components/Project.js
@@ -7,9 +7,8 @@ const Project = () => {
       <div className="projects__content">
         <div className="project__title"> Projects </div>
         <p className="project__description">
-          Here you will find some of the personal and 
-          clients projects that I created with each project 
-          containing its own case study
+          Here you will find some of the personal projects that I have created with each project 
+          containing its own source code button
         </p>
 
         <div className="sinlge__project-container">
@@ -17,24 +16,29 @@ const Project = () => {
           <div className="project__mini-container">
             <div className="single__project__title"> Portfolio</div>
             <p className="single__project__description">
-              Dopefolio is a successful Open-Source project that I created 
-              which have been featured on some of the biggest tech sites like 
-              CSS-Tricks, Hostinger, etc & used by thousands of developers globally
+            This is a single-page React app showcasing my work, skills, and contact information. 
+            The portfolio is hosted on Digital Ocean, and the contact form is powered by email.js
             </p>
-            <button className="case__study__btn"> source code </button>
+            <button className="case__study__btn"> 
+              <a href="https://github.com/solohuck/React-Portfolio">source code</a>
+            </button>
           </div>
         </div>
 
         <div className="sinlge__project-container">
-          <div className="project__container_"></div>  
+          <div className="project__container_">
+            <div className="lock__img"></div>
+          </div>  
           <div className="project__mini-container">
-            <div className="single__project__title"> Portfolio</div>
+            <div className="single__project__title"> Mental Math </div>
             <p className="single__project__description">
               Dopefolio is a successful Open-Source project that I created 
               which have been featured on some of the biggest tech sites like 
               CSS-Tricks, Hostinger, etc & used by thousands of developers globally
             </p>
-            <button className="case__study__btn"> source code </button>
+            <button className="case__study__btn"> 
+              <a href="https://github.com/solohuck/javaquiz-2">source code</a> 
+            </button>
           </div>
         </div>
       </div>
